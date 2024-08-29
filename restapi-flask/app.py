8c148369edec4325881bf03fb52358431e4aa981
igor-primo <igorprimo62@gmail.com>
2024-08-29 20:55:08 -0300

    feat: add flask restful initial code

--- restapi-flask/app.py
+++ restapi-flask/app.py
@@ -1,6 +1,22 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Resource, Api
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    def get(self):
+        return {'message': 'user 1'}
+
+class User(Resource):
+    def post(self):
+        return {'message': 'teste'}
+
+    def get(self, cpf):
+        return {'message': 'CPF'}
+
+api.add_resource(Users, '/users')
+api.add_resource(User, '/user' , '/users/<string:cpf>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
