90452ccaeaa61687c194ccaa2371ed07ef3816e0
igor-primo <igorprimo62@gmail.com>
2024-09-01 11:56:56 -0300

    feat: add ability to connect to db

--- restapi-flask/app.py
+++ restapi-flask/app.py
@@ -1,8 +1,25 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 api = Api(app)
+
+app.config['MONGODB_SETTINGS'] = {
+    "db": "users",
+    "host": "mongodb",
+    "port": 27017,
+    "username": "admin",
+    "password": "admin"
+}
+db = MongoEngine(app)
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    email = db.StringField(required=True)
+    first_name = db.StringField(required=True, max_length=50)
+    last_name = db.StringField(required=True, max_length=50)
+    birth_date = db.DateTimeField(required=True)
 
 class Users(Resource):
     def get(self):
