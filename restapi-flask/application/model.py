67debf3c57aa45562e7fa27ec6e1fddf24b5762a
igor-primo <igorprimo62@gmail.com>
2024-09-09 13:25:40 -0300

    fix: start refactoring

---
+++ restapi-flask/application/model.py
@@ -0,0 +1,9 @@
+from .db import db
+
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    email = db.StringField(required=True)
+    first_name = db.StringField(required=True, max_length=50)
+    last_name = db.StringField(required=True, max_length=50)
+    birth_date = db.DateTimeField(required=True)
