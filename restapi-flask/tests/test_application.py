4e7a20c51c7092d11c61999cfae0a5ab7e7bb951
igor-primo <igorprimo62@gmail.com>
2024-09-10 11:03:49 -0300

    test: add user post test

--- restapi-flask/tests/test_application.py
+++ restapi-flask/tests/test_application.py
@@ -9,6 +9,35 @@ class TestApplication:
         app = create_app("config.MockConfig")
         return app.test_client()
 
+    @pytest.fixture
+    def valid_user(self):
+        return {
+            "first_name": "igor",
+            "last_name": "primo",
+            "cpf": "427.510.852-37",
+            "email": "igorprimo62@gmail.com",
+            "birth_date": "1999-10-20",
+        }
+
+    @pytest.fixture
+    def invalid_user(self):
+        return {
+            "first_name": "igor",
+            "last_name": "primo",
+            "cpf": "42751085232",
+            "email": "igorprimo62@gmail.com",
+            "birth_date": "1999-10-20",
+        }
+
     def test_get_users(self, client):
         response = client.get("/users")
         assert response.status_code == 200
+
+    def test_post_user(self, client, valid_user, invalid_user):
+        response = client.post("/user", json=valid_user)
+        assert response.status_code == 200
+        assert b"successfully" in response.data
+
+        response = client.post("/user", json=invalid_user)
+        assert response.status_code == 400
+        assert b"invalid" in response.data
