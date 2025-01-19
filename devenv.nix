e05d2b447ad806daff05e643c0a2a43c2c9a9c60
igor-primo <igorprimo62@gmail.com>
2025-01-18 22:57:08 -0300

    chore: add script to facilitate login into the machine

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = [ 
+  packages = [
     pkgs.git
     pkgs.insomnia
     pkgs.kind
@@ -25,6 +25,10 @@
   scripts.k.exec = ''kubectl "$@"'';
   scripts.ta.exec = ''terraform apply -auto-approve'';
   scripts.td.exec = ''terraform destroy -auto-approve'';
+  scripts.gip.exec = ''
+    sudo virsh domifaddr ubuntu-qcow2 | tail -n 2 | head -n 1 | awk '{print $4}' | tr '/' '\n' | head -n 1
+  '';
+  scripts.dossh.exec = ''ssh ubuntu@$(gip)'';
 
   pre-commit.hooks.black.enable = true;
 }
